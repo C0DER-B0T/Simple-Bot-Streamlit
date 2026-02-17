be0e3a4aec99a42e01e5912ae64f2c225910cdbf
C0DER-b0t <chandrasatyam31@gmail.com>
2026-02-17 18:45:22 +0530

    Revert "Add Simple UI using Streamlit"
    This reverts commit 02b9571a3fba9ceb77fe3ebcd7c7271bdc60b755.

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 from google import genai
 import os
 from dotenv import load_dotenv
-import streamlit as st
+
 load_dotenv()
 
+# 2. Retrieve the variable using os.getenv()
 api = os.getenv("MY_SECRET_API_KEY")
 
-def get(user_input):
-    ai = genai.Client(api_key=api)
+api= "AIzaSyBqV0eGY_Q10_As21LxuqU-nZ1daDzAGh4"
 
-    response= ai.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=user_input
-    )
-    return response.text
+ai = genai.Client(api_key=api)
 
-st.title("AI Chat BOt")
+response= ai.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="how to get more likes ?"
+)
 
-user_inp=st.chat_input("Ask me anything...")
-
-if user_inp :
-    with st.chat_message("User"):
-        st.markdown(user_inp)
-    with st.chat_message("AI"):
-        with st.spinner("Thinking..."):
-            st.markdown(get(user_inp))
+print(response.text)
